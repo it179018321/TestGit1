cdbbe7e621fdd96688115205dd3a3a586ab025e8
admin <ITzhaoqi>
2020-06-09 20:28:17 +0800

    冲突测试1.1

--- src/main/java/demo01.java
+++ src/main/java/demo01.java
@@ -2,6 +2,6 @@ public class demo01 {
 
 
     public static void main(String[] args) {
-        System.out.println("1111");
+        System.out.println("111100000");
     }
 }
--- src/main/java/demo02.java
+++ src/main/java/demo02.java
@@ -1,5 +1,5 @@
 public class demo02 {
     public static void main(String[] args) {
-        System.out.println("222");
+        System.out.println("2223333333");
     }
 }
