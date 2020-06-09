20ded9a275df07b21c4c76e4158d6972e5c59a1a
admin <ITzhaoqi>
2020-06-09 20:53:49 +0800

    1111

--- src/main/java/demo01.java
+++ src/main/java/demo01.java
@@ -3,6 +3,6 @@ public class demo01 {
 
     public static void main(String[] args) {
         System.out.println("111100000");
-        System.out.println("1111444");
+        System.out.println("111144400000");
     }
 }
